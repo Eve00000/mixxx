4c70c0a757c048de8443f61e5e55e0ca38367348
Evelynne <Evelynne@Evelynne-Veys.com>
2024-11-11 15:13:03 +0100

    whereclause & narrowing operator choice

--- src/widget/wtracktableview.cpp
+++ src/widget/wtracktableview.cpp
@@ -610,6 +610,8 @@ void WTrackTableView::onSearch(const QString& text) {
     saveCurrentViewState();
     bool queryIsLessSpecific = SearchQueryParser::queryIsLessSpecific(
             pTrackModel->currentSearch(), text);
+    qDebug() << "[WTrackTableView] [SEARCH] -> result " << text;
+    qDebug() << "[WTrackTableView] [SEARCH] -> result " << pTrackModel->currentSearch(), text;
     QList<TrackId> selectedTracks = getSelectedTrackIds();
     TrackId prevTrack = getCurrentTrackId();
     saveCurrentIndex();
