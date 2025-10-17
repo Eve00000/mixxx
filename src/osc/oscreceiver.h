17739ce014c696416efcb1803ed79ab9a4a7464a
Evelynne <evelynne@protonmail.com>
2025-10-17 17:01:20 +0200

    Fix unused functions in oscreceiver.h

--- src/osc/oscreceiver.h
+++ src/osc/oscreceiver.h
@@ -63,16 +63,16 @@ class OscReceiver : public QObject {
 };
 
 // Static callback functions
-static void errorCallback(int num, const char* msg, const char* where);
-static int quit_handler(const char* path,
-        const char* types,
-        lo_arg** argv,
-        int argc,
-        lo_message data,
-        void* user_data);
-static int messageCallback(const char* path,
-        const char* types,
-        lo_arg** argv,
-        int argc,
-        lo_message data,
-        void* user_data);
+// static void errorCallback(int num, const char* msg, const char* where);
+// static int quit_handler(const char* path,
+//        const char* types,
+//        lo_arg** argv,
+//        int argc,
+//        lo_message data,
+//        void* user_data);
+// static int messageCallback(const char* path,
+//        const char* types,
+//        lo_arg** argv,
+//        int argc,
+//        lo_message data,
+//        void* user_data);
