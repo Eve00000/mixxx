f67d3cccaa93762586464f8901441b0647239f42
Evelynne <evelynne@protonmail.com>
2026-03-09 15:20:39 +0100

    disable liblo test, solve error in wtrackstemmenu

--- src/widget/wtrackstemmenu.cpp
+++ src/widget/wtrackstemmenu.cpp
@@ -67,10 +67,12 @@ WTrackStemMenu::WTrackStemMenu(const QString& label,
 
 bool WTrackStemMenu::eventFilter(QObject* pObj, QEvent* e) {
     QInputEvent* pInputEvent = dynamic_cast<QInputEvent*>(e);
-    if (pInputEvent != nullptr &&
-            (pInputEvent->modifiers() & Qt::ControlModifier) != m_selectMode) {
-        m_selectMode = pInputEvent->modifiers() & Qt::ControlModifier;
-        updateActions();
+    if (pInputEvent != nullptr) {
+        bool selectMode = pInputEvent->modifiers().testFlag(Qt::ControlModifier);
+        if (selectMode != m_selectMode) {
+            m_selectMode = selectMode;
+            updateActions();
+        }
     }
 
     if (m_selectMode && (e->type() == QEvent::MouseButtonRelease)) {
